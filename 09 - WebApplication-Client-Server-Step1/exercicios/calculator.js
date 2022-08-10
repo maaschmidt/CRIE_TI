3ee4fb7aad5393a93045bc3b0faa0737bf1c5236
maaschmidt <marcel.schmidt@universo.univates.br>
2022-08-10 09:20:39 -0300

    update calculator

--- 09 - WebApplication-Client-Server-Step1/exercicios/calculator.js
+++ 09 - WebApplication-Client-Server-Step1/exercicios/calculator.js
@@ -1,15 +1,53 @@
 var result = document.querySelector("#result");
 var num1 = 0;
 var num2 = 0;
-var funcao;
+var funcao = '';
+
 function numeroDigitado(numero){
+    if(num1 != 0){
+        this.num2 = numero;
+        // console.log(num2);
+    }
     var div = document.createElement("div");
     div.innerHTML = numero;
     result.appendChild(div);
 }
 
 function operacao(funcao){
-    console.log(funcao)
+    this.num1 = result.textContent;
+    let div = document.createElement("div");
+    div.innerHTML = funcao;
+    result.appendChild(div);
     this.funcao = funcao;
-    result.appendChild(funcao);
+}
+
+function numeroDecimal(ponto){
+    let div = document.createElement("div");
+    div.innerHTML = ponto;
+    result.appendChild(div);
+}
+
+function limpar(){
+    result.innerHTML = "";
+    num1 = 0;
+    num2 = 0;
+    funcao = '';
+}
+
+function deletaUltimo(){
+    result.removeChild(result.lastChild);
+}
+
+function igualA(){
+    let resultado;
+    if (funcao == "/"){
+        resultado = num1/num2;
+    } else if (funcao == "*"){
+        resultado = num1*num2;
+    } else if (funcao == "-"){
+        resultado = num1-num2;
+    } else if (funcao == "+"){
+        resultado = num1+num2;
+    }
+    result.innerHTML = resultado;
 }
